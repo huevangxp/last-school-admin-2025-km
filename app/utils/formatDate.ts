6208bb04bcf5165b4ecf89772e27d1862bb1f7d4
huevangxp <huevang933@gmail.com>
2025-12-17 10:38:14 +0700

    commit Wed 12/17/2025 10:38:14.12

--- app/utils/formatDate.ts
+++ app/utils/formatDate.ts
@@ -8,6 +8,6 @@ export const formatDateTime = (date: string) => {
     return d.toLocaleDateString() + " " + d.toLocaleTimeString();
 };
 
-export const hiddenId = (id: string) => {
+export const hiddenId = (id: any) => {
     return id.slice(0, 3) + "*****" + id.slice(-3);
 };
