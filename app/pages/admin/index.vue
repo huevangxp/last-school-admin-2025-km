9739aa26601497076f8a910fb5731f77f138df9b
huevangxp <huevang933@gmail.com>
2026-03-04 15:37:14 +0700

    commit Wed 03/04/2026 15:37:14.13

--- app/pages/admin/index.vue
+++ app/pages/admin/index.vue
@@ -7,10 +7,12 @@
       class="d-flex flex-column flex-md-row align-md-center justify-space-between mb-6"
     >
       <div>
-        <h1 class="text-title mb-1">{{ t("management") }} {{ t("admin") }}</h1>
-        <p class="text-detail">
+        <div class="text-title mb-1">
+          {{ t("management") }} {{ t("admin") }}
+        </div>
+        <div class="text-detail">
           Manage system administrators and security permissions.
-        </p>
+        </div>
       </div>
 
       <div class="d-flex gap-2 align-center flex-wrap mt-4 mt-md-0">
