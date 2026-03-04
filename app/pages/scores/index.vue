f0f820599559fa95b302ac5b0106917c60f9b599
huevangxp <huevang933@gmail.com>
2026-03-04 15:37:26 +0700

    commit Wed 03/04/2026 15:37:26.15

--- app/pages/scores/index.vue
+++ app/pages/scores/index.vue
@@ -7,10 +7,10 @@
       class="d-flex flex-column flex-md-row align-md-center justify-space-between mb-6"
     >
       <div>
-        <h1 class="text-title mb-1">
+        <div class="text-title mb-1">
           {{ $t("management") }} {{ $t("scores") }}
-        </h1>
-        <p class="text-detail">Manage student grades and performance.</p>
+        </div>
+        <div class="text-detail">Manage student grades and performance.</div>
       </div>
 
       <div class="d-flex gap-2 align-center flex-wrap mt-4 mt-md-0">
