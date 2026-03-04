13b40e1a549c8b129eeddfe9ac68432728943889
huevangxp <huevang933@gmail.com>
2026-03-04 15:36:36 +0700

    commit Wed 03/04/2026 15:36:36.16

--- app/pages/teachers/index.vue
+++ app/pages/teachers/index.vue
@@ -7,12 +7,12 @@
       class="d-flex flex-column flex-md-row align-md-center justify-space-between mb-6"
     >
       <div>
-        <h1 class="text-title mb-1">
+        <div class="text-title mb-1">
           {{ t("management") }} {{ t("teachers") }}
-        </h1>
-        <p class="text-detail">
+        </div>
+        <div class="text-detail">
           Manage your faculty members and their assignments.
-        </p>
+        </div>
       </div>
 
       <div class="d-flex gap-2 align-center flex-wrap mt-4 mt-md-0">
