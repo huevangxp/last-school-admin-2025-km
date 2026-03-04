5bbcb59990f920504e7a12fdfe6a211d31cdf136
huevangxp <huevang933@gmail.com>
2026-03-04 15:37:01 +0700

    commit Wed 03/04/2026 15:37:01.16

--- app/pages/academic/index.vue
+++ app/pages/academic/index.vue
@@ -7,8 +7,8 @@
       class="d-flex flex-column flex-md-row align-md-center justify-space-between mb-6"
     >
       <div>
-        <h1 class="text-title mb-1">Academic Management</h1>
-        <p class="text-detail">Manage academic years and terms</p>
+        <div class="text-title mb-1">Academic Management</div>
+        <div class="text-detail">Manage academic years and terms</div>
       </div>
 
       <div class="d-flex gap-2 align-center flex-wrap mt-4 mt-md-0">
