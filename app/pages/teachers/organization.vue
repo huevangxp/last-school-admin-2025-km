f5b0f0652cd78458c01b1d620e86b091650893f5
huevangxp <huevang933@gmail.com>
2026-07-06 16:19:52 +0700

    commit Mon 07/06/2026 16:19:52.18

--- app/pages/teachers/organization.vue
+++ app/pages/teachers/organization.vue
@@ -102,6 +102,7 @@ const teachers = computed(() =>
     status: capitalize(tc.status),
     position: tc.position || "",
     department: tc.department || "",
+    layer: tc.layer ?? null,
     managerId: tc.manager_id || null,
   }))
 );
