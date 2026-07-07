37dc5f2a1f823ef53249c4c7e6158fe221703268
huevangxp <huevang933@gmail.com>
2026-07-07 14:21:06 +0700

    commit Tue 07/07/2026 14:21:06.20

--- app/pages/class/index.vue
+++ app/pages/class/index.vue
@@ -159,9 +159,7 @@
         <template v-slot:item.teacher="{ item }">
           <div class="d-flex align-center">
             <v-avatar size="32" class="mr-2 elevation-1">
-              <!-- <v-img
-                :src="`https://ui-avatars.com/api/?name=${item.teacher}&background=random`"
-              ></v-img> -->
+             
             </v-avatar>
             <div>
               <div class="text-title-small">{{ item.teacher }}</div>
