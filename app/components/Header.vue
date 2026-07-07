4f291ee2841de2bcc596a03e943d2a74e4fe2f6e
huevangxp <huevang933@gmail.com>
2026-07-07 16:16:43 +0700

    commit Tue 07/07/2026 16:16:43.19

--- app/components/Header.vue
+++ app/components/Header.vue
@@ -444,6 +444,7 @@ const allMenuSections: MenuSection[] = [
     items: [
       { title: "news", icon: "mdi-newspaper-variant-outline", to: "/news" },
       { title: "activities", icon: "mdi-calendar-star", to: "/activities" },
+      { title: "gallery", icon: "mdi-image-multiple-outline", to: "/gallery" },
       { title: "new_academic_year", icon: "mdi-calendar-plus", to: "/academic/add" },
     ],
   },
