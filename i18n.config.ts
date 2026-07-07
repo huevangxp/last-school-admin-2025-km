43404f4affd5f99813541bbb6f91587fbeec9dd3
huevangxp <huevang933@gmail.com>
2026-07-07 12:59:42 +0700

    commit Tue 07/07/2026 12:59:42.17

---
+++ i18n.config.ts
@@ -0,0 +1,6 @@
+// vue-i18n options for @nuxtjs/i18n. Locale messages themselves are loaded from
+// the per-locale JSON files declared in nuxt.config (i18n.locales[].file).
+export default defineI18nConfig(() => ({
+  legacy: false,
+  fallbackLocale: "en",
+}));
