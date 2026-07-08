1f5b3fa624271856b04ac5bd0e656fc959d0e9b1
huevangxp <huevang933@gmail.com>
2026-07-08 10:59:38 +0700

    commit Wed 07/08/2026 10:59:38.19

---
+++ app/components/DashboardPieChart.vue
@@ -0,0 +1,75 @@
+<template>
+  <Pie :data="chartData" :options="chartOptions" />
+</template>
+
+<script setup lang="ts">
+import { computed } from "vue";
+import {
+  Chart as ChartJS,
+  ArcElement,
+  Title,
+  Tooltip,
+  Legend,
+} from "chart.js";
+import { Pie } from "vue-chartjs";
+
+ChartJS.register(ArcElement, Title, Tooltip, Legend);
+
+const PALETTE = [
+  "#14b8a6",
+  "#448AFF",
+  "#6366f1",
+  "#f59e0b",
+  "#ec4899",
+  "#10b981",
+  "#8b5cf6",
+  "#ef4444",
+  "#0ea5e9",
+  "#a3e635",
+  "#f97316",
+  "#64748b",
+];
+
+const props = withDefaults(
+  defineProps<{
+    labels?: string[];
+    values?: number[];
+  }>(),
+  {
+    labels: () => [],
+    values: () => [],
+  }
+);
+
+const chartData = computed(() => ({
+  labels: props.labels,
+  datasets: [
+    {
+      data: props.values,
+      backgroundColor: props.labels.map((_, i) => PALETTE[i % PALETTE.length]),
+      borderColor: "#fff",
+      borderWidth: 2,
+    },
+  ],
+}));
+
+const chartOptions: any = {
+  responsive: true,
+  maintainAspectRatio: false,
+  plugins: {
+    legend: {
+      display: true,
+      position: "right",
+      labels: { usePointStyle: true, boxWidth: 8, font: { size: 11 } },
+    },
+    tooltip: {
+      backgroundColor: "#fff",
+      titleColor: "#333",
+      bodyColor: "#666",
+      borderColor: "#eee",
+      borderWidth: 1,
+      padding: 10,
+    },
+  },
+};
+</script>
