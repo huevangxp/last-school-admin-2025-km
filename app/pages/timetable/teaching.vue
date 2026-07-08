fe93597a89b9a1fe0a095db39db6a776467a2403
huevangxp <huevang933@gmail.com>
2026-07-08 15:39:35 +0700

    commit Wed 07/08/2026 15:39:35.19

---
+++ app/pages/timetable/teaching.vue
@@ -0,0 +1,6 @@
+<template>
+  <!-- Teaching schedule: one teacher's weekly timetable across their classes. -->
+  <TimetableBoard mode="teaching" />
+</template>
+
+<script setup lang="ts"></script>
