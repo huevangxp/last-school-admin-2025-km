c5c0e4ebfafe5586db2fe320815625bc34a91e49
huevangxp <huevang933@gmail.com>
2026-07-08 13:47:45 +0700

    commit Wed 07/08/2026 13:47:45.11

--- app/pages/teachers/index.vue
+++ app/pages/teachers/index.vue
@@ -485,7 +485,7 @@
 </template>
 
 <script setup lang="ts">
-import { ref, computed, onMounted } from "vue";
+import { ref, computed, watch, onMounted } from "vue";
 import {
   positionOptions,
   departmentOptions,
