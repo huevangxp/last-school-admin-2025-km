a50de297ee1a7f55fa45de3e7a9ee6b11dba3867
huevangxp <huevang933@gmail.com>
2026-07-08 15:21:21 +0700

    commit Wed 07/08/2026 15:21:21.14

--- app/pages/timetable/index.vue
+++ app/pages/timetable/index.vue
@@ -276,7 +276,7 @@
                     <span class="font-weight-bold text-teal-darken-2">{{
                       opt.subject
                     }}</span>
-                    · {{ opt.teacher }}
+                    · {{ opt.secondary }}
                   </v-list-item-title>
                 </v-list-item>
                 <template v-if="cellFor(p.id, currentDayKey)">
