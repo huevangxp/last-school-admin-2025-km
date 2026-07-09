9057754a415f705e67d908304c52a4c81f5eb282
huevangxp <huevang933@gmail.com>
2026-07-09 10:48:09 +0700

    commit Thu 07/09/2026 10:48:09.16

--- app/pages/students/index.vue
+++ app/pages/students/index.vue
@@ -8,6 +8,7 @@
           color="white"
           class="modern-action-btn secondary border"
           height="36"
+          @click="exportList"
         >
           <v-icon icon="mdi-file-download-outline" start size="18"></v-icon>
           {{ t("export") }}
