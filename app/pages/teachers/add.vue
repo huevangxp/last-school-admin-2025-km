581c42d00b354dab8847fb68d237928a2c277cb7
huevangxp <huevang933@gmail.com>
2026-07-09 11:02:34 +0700

    commit Thu 07/09/2026 11:02:34.20

--- app/pages/teachers/add.vue
+++ app/pages/teachers/add.vue
@@ -424,7 +424,7 @@ const save = async () => {
       department: f.department || null,
       layer: f.layer || null,
       manager_id: f.managerId || null,
-      avatar: f.avatar || "https://picsum.photos/200",
+      avatar: f.avatar || null,
       status: "active",
     });
     router.push("/teachers");
